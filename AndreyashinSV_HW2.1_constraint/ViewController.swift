fb7fc52fab9a337bacb73c25a599cdd6c7ce576a
sa <sa@wrf.su>
2020-02-26 13:25:01 +0300

    Initial Commit

---
+++ AndreyashinSV_HW2.1_constraint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AndreyashinSV_HW2.1_constraint
+//
+//  Created by lastbyte on 26.02.2020.
+//  Copyright © 2020 lastbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
